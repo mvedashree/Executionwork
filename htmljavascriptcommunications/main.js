a9404f8657e3f3d576053cb20272de665883ea95
veda <mveda22shree82004@gmail.com>
2026-01-29 12:54:48 +0530

    code

---
+++ htmljavascriptcommunications/main.js
@@ -0,0 +1,24 @@
+// function ready(){
+//     // mydiv=document.getElementById("mydiv")
+//     // mydiv.innerText="raaga"
+//     // mydiv.innerHTML="<b><h1>i am veda</h1></b>"
+//     console.log(mydiv.innerText); 
+// }
+
+// function ready(){
+//     let email=document.getElementById("email")
+//     // console.log(email);
+//     // console.log(email.name);
+//     // console.log(email.value);
+//     // console.log(email.id);
+//     email.type="password"     
+// }
+
+function ready(){
+    let btn=document.getElementById("btn")
+    btn.addEventListener("click",save())
+}
+ function save(){
+    console.log("i am save fun");
+ }
+ 
